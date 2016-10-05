9f7ca724b71fcfabba2be127554c32b258ce3532
Sebastian Guenther <sebastian.guenther@ifes.uni-hannover.de>
2016-10-05 11:43:40 +0200

    [interpt] + interpoint calculation, * main, hybrid
    Added functionality to calculate a pair of base/peak storages within the leaf.
    For this, a ratio inter was defined, determining which amount of signal is
    handed to the peak storage before it goes to the base storage. This leads to an
    increase of energy of the peak storage, while retaining the original power cut
    ratio. With this, every pair of points within the leaf can be reached but it
    can not be predicted which pair will be met with a certain inter value.
    
    calc_hybrid_storage: * base and signal fcn def, + fcn input args (inter added)
    main: + fcn input args (inter added), call to calc_hybrid_storage complemented
    
    On branch feature/interpt
    Changes to be committed:
          modified:   project/calc_hybrid_storage.m
          modified:   project/main.m

--- project/main.m
+++ project/main.m
@@ -1,4 +1,4 @@
-function out = main(signal, calc_max_step, calc_max_tol, cut_off_vector)
+function out = main(signal, inter, calc_max_step, calc_max_tol, cut_off_vector)
 % MAIN Gather main functionality of hybrid leaf tool
 %
 % Calculates energy and power for hybrid storage systems containing a base
@@ -7,6 +7,11 @@ function out = main(signal, calc_max_step, calc_max_tol, cut_off_vector)
 % 
 % Input:
 %   signal          struct, see issignalstruct
+%   inter           optional, default 0, calculates hybrid storage with an
+%                   intermediate point strategy within the leaf, as a fraction
+%                   of p_cut_ratio
+%                   0.. original boundary of leaf
+%                   1.. single storage e/p iso time line
 %   calc_max_step   maximum integration step size
 %                   optional, default 1e-2
 %   calc_max_tol    maximum allowed tolerance between hybrid and single
@@ -33,12 +38,15 @@ function out = main(signal, calc_max_step, calc_max_tol, cut_off_vector)
 assert(issignalstruct(signal), 'Invalid input - no signal struct')
 
 if nargin < 2
-    calc_max_step = 1e-2;
+    inter = 0;
 end
 if nargin < 3
-    calc_max_tol = 1e-2;
+    calc_max_step = 1e-2;
 end
 if nargin < 4
+    calc_max_tol = 1e-2;
+end
+if nargin < 5
     cut_off_fcn = @(x) x - 0.12*sin(2*pi*x);
     cut_off_vector = cut_off_fcn([0:0.1:1]');
 end
@@ -53,7 +61,7 @@ end
 hybrid_table = [cut_off_vector, zeros(length(cut_off_vector), 4)];
 for ii = 1:length(cut_off_vector)
     cut_off = cut_off_vector(ii);
-    [base, peak] = calc_hybrid_storage(signal, cut_off, calc_max_step);
+    [base, peak] = calc_hybrid_storage(signal, cut_off, inter, calc_max_step);
     hybrid_table(ii,:) = [cut_off, ...
                           base.energy, peak.energy, ...
                           base.power, peak.power];
