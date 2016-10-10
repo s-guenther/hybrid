bb04e31bc3eae664cf2bed4fc99351ad10db1a55
Sebastian Guenther <sebastian.guenther@ifes.uni-hannover.de>
2016-10-10 11:38:57 +0200

    [reload] + hybrid calc as dae w/o working op strat
    Added function which calcs a hybrid storage system as a dae system to study
    operational strategies which consider inter storage reloading. Atm, the
    operational strategy does not work properly, but the old strategy w/o reloading
    works as a dae formulation.
    
    On branch feature/reload
    Changes to be committed:
          new file:   project/auxiliary/op_strat_reload_dim.m
          new file:   project/calc_hybrid_w_reload.m

---
+++ project/auxiliary/op_strat_reload_dim.m
@@ -0,0 +1,28 @@
+function p_base = op_strat_reload_dim(p_in, t, p_base_max, e_peak, period)
+% OP_STRAT_RELOAD_DIM time dependend operational strat for dimensioning
+%
+% Is a simple operational strategy with an a priori knowledge of period and
+% signal symmetry. With this, the storage system can be simulated without the
+% knowledge about the dimensions. Determining them, is task of this operational
+% strategy.
+%
+% Input:
+%   p_in        power input (to both storages/storage system)
+%   t           simulation time
+%   p_base_max  maximum possible power of the base storage
+%   period      period of input signal
+%
+% Output:
+%   p_base      base power output
+%   
+% The first half period the system is in 'charge mode', the second half in
+% 'discharge mode'
+
+T = period;
+p_base = -p_in       .*(p_in >= 0 & abs(p_in) <= p_base_max & t <= T/2) + ...
+         -p_base_max .*(p_in >= 0 & abs(p_in) >= p_base_max & t <= T/2) + ...
+         -p_in       .*(p_in < 0 & abs(p_in) <= p_base_max & t > T/2) + ...
+         p_base_max  .*(p_in < 0 & abs(p_in) >= p_base_max & t > T/2);
+        
+
+end
---
+++ project/calc_hybrid_w_reload.m
@@ -0,0 +1,54 @@
+function [base, peak] = calc_hybrid_w_reload(signal, p_cut_ratio, ...
+                                             inter, max_step)
+% CALC_HYBRID_W_RELOAD simulation w/ inter storage power flow
+%
+% Calculates for a point symmetric input signal and a given power ratio the
+% energy and power of the peak and base storage, considering inter storage
+% power flow. The strategy tries to minimize energy of peak storage.
+%
+% Input:
+%   signal          signal struct, see issignalstruct
+%   p_cut_ratio     power ratio, where peak begins to work, between 0..1
+%   inter           optional, default 0, calculates hybrid storage with an
+%                   intermediate point strategy within the leaf, as a fraction
+%                   of p_cut_ratio, partial reloading of maximum possible
+%                   0.. maximum possible reload strategy
+%                   1.. original boundary of leaf
+%   max_step        optional, default 1e-2, max integration step size
+%
+% Output:
+%   base.energy     energy of base storage
+%   base.power      power of base storage
+%   peak.energy     energy of peak storage
+%   peak.power      power of peak storage
+
+assert(issignalstruct(signal), 'Invalid input - no signal struct')
+
+if nargin < 3
+    inter = 0;
+end
+if nargin < 4
+    max_step = 1e-2;
+end
+
+% Define storage odes
+dedt_base = @(p_base) -p_base;
+dedt_peak = @(p_peak) -p_peak;
+
+% extract signal info
+p_in = signal.fcn;
+period = signal.period;
+p_base_max = signal.amplitude*p_cut_ratio;
+p_base = @(t, e_peak) op_strat_reload_dim(p_in(t), t, p_base_max, e_peak, period);
+
+% Construct dae
+% y1.. e_base, y2.. e_peak, y3.. dae for p_base, y4.. dae for p_peak
+dae = @(t,y) [dedt_base(p_base(t, y(2)));
+              dedt_base(y(3));
+              p_in(t) + p_base(t, y(2)) + y(3)];
+
+% Set up for dae solving
+opt = odeset('MaxStep', max_step, 'mass', diag([1, 1, 0]));
+[t, y] = ode15s(dae, [0 signal.period], [0, 0, 0], opt);
+
+end%mainfcn
